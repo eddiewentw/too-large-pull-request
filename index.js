f496363ec31bd0a4efde0699af5c884e5240b46c
Eddie Wen <eddiewen.tw@gmail.com>
2020-02-22 22:24:47 +1300

    fix: correct event action name
    FIX ISSUE #7

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ module.exports = app => {
 	app.on([
 		'pull_request.opened',
 		'pull_request.reopened',
-		'pull_request.synchronized',
+		'pull_request.synchronize',
 		'pull_request.edited',
 	], async context => {
 		const pullRequest = context.payload.pull_request;
